8a0a5b4e0e394d3f2d37b36faf8a477e73324639
Seungmin-Yang <mse9719@gmail.com>
2025-05-06 15:01:40 +0900

    feat:히든 npc 구현 및 말걸기 구현
    feat:히든 npc 구현 및 말걸기 구현
    TODO:히든 게임 구현

---
+++ Assets/Scripts/LobbySceneScript/Manager/HiddenNPCManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class HiddenNPCManager : MonoBehaviour
+{
+    [SerializeField] private Button yesButton;
+    [SerializeField] private Button noButton;
+    [SerializeField] private GameObject canvas;
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        yesButton.onClick.AddListener(OnClickHiddenButton);
+        noButton.onClick.AddListener(OnClickQuitButton);
+    }
+
+    public void OnClickHiddenButton()
+    {
+        SceneManager.LoadScene("HiddenGameScene");
+    }
+
+    public void OnClickQuitButton()
+    {
+        canvas.SetActive(false);
+    }
+
+    public void TalkNPC()
+    {
+        canvas.SetActive(true);
+    }
+}
